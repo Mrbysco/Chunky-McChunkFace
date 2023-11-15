c194dfaa01eea024e5e4f6e123bbc6b39ea91a3d
Mrbysco <Mrbysco@users.noreply.github.com>
2023-11-15 16:21:17 +0100

    Update Jade

--- src/main/java/com/mrbysco/chunkymcchunkface/compat/jade/JadePlugin.java
+++ src/main/java/com/mrbysco/chunkymcchunkface/compat/jade/JadePlugin.java
@@ -4,17 +4,16 @@ import com.mrbysco.chunkymcchunkface.ChunkyMcChunkFace;
 import com.mrbysco.chunkymcchunkface.blocks.ChunkLoaderBlock;
 import com.mrbysco.chunkymcchunkface.blocks.entity.ChunkLoaderBlockEntity;
 import com.mrbysco.chunkymcchunkface.config.ChunkyConfig;
-import mcp.mobius.waila.api.BlockAccessor;
-import mcp.mobius.waila.api.IComponentProvider;
-import mcp.mobius.waila.api.ITooltip;
-import mcp.mobius.waila.api.IWailaClientRegistration;
-import mcp.mobius.waila.api.IWailaCommonRegistration;
-import mcp.mobius.waila.api.IWailaPlugin;
-import mcp.mobius.waila.api.TooltipPosition;
-import mcp.mobius.waila.api.WailaPlugin;
-import mcp.mobius.waila.api.config.IPluginConfig;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.IWailaClientRegistration;
+import snownee.jade.api.IWailaCommonRegistration;
+import snownee.jade.api.IWailaPlugin;
+import snownee.jade.api.WailaPlugin;
+import snownee.jade.api.config.IPluginConfig;
 
 @WailaPlugin
 public class JadePlugin implements IWailaPlugin {
@@ -22,15 +21,16 @@ public class JadePlugin implements IWailaPlugin {
 
 	@Override
 	public void register(IWailaCommonRegistration registration) {
-		registration.addConfig(SHOW_TIME, true);
+
 	}
 
 	@Override
 	public void registerClient(IWailaClientRegistration registration) {
-		registration.registerComponentProvider(ChunkLoaderTimeProvider.INSTANCE, TooltipPosition.BODY, ChunkLoaderBlock.class);
+		registration.addConfig(SHOW_TIME, true);
+		registration.registerBlockComponent(ChunkLoaderTimeProvider.INSTANCE, ChunkLoaderBlock.class);
 	}
 
-	public enum ChunkLoaderTimeProvider implements IComponentProvider {
+	public enum ChunkLoaderTimeProvider implements IBlockComponentProvider {
 
 		INSTANCE;
 
@@ -38,7 +38,7 @@ public class JadePlugin implements IWailaPlugin {
 		public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig pluginConfig) {
 			if (pluginConfig.get(SHOW_TIME) && blockAccessor.getBlockEntity() instanceof ChunkLoaderBlockEntity blockEntity) {
 				if (blockEntity.getPlayerOnlineCache()) {
-					tooltip.add(new TranslatableComponent("chunkymcchunkface.waila.time.online"));
+					tooltip.add(Component.translatable("chunkymcchunkface.waila.time.online"));
 				} else {
 					final int configuredTicks = ChunkyConfig.COMMON.offlineTime.get();
 					if (configuredTicks != 0) {
@@ -48,12 +48,17 @@ public class JadePlugin implements IWailaPlugin {
 						int ticksLeft = configuredTicks - (int) ticksActive;
 						int seconds = ticksLeft / 20;
 
-						tooltip.add(new TranslatableComponent("chunkymcchunkface.waila.time.remaining", seconds));
+						tooltip.add(Component.translatable("chunkymcchunkface.waila.time.remaining", seconds));
 					} else {
-						tooltip.add(new TranslatableComponent("chunkymcchunkface.waila.time.disabled"));
+						tooltip.add(Component.translatable("chunkymcchunkface.waila.time.disabled"));
 					}
 				}
 			}
+		}
+
+		@Override
+		public ResourceLocation getUid() {
+			return JadePlugin.SHOW_TIME;
 		}
 	}
 }
