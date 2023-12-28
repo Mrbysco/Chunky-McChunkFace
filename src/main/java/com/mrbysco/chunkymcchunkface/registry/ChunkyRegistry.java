2193901f102b78cde82ec8e355a66fc8abcd444f
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-28 19:06:26 +0100

    Don't allow piston movement

--- src/main/java/com/mrbysco/chunkymcchunkface/registry/ChunkyRegistry.java
+++ src/main/java/com/mrbysco/chunkymcchunkface/registry/ChunkyRegistry.java
@@ -9,6 +9,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -21,7 +22,7 @@ public class ChunkyRegistry {
 	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ChunkyMcChunkFace.MOD_ID);
 
 	public static final DeferredBlock<ChunkLoaderBlock> CHUNK_LOADER = BLOCKS.register("chunk_loader", () ->
-			new ChunkLoaderBlock(Block.Properties.of().mapColor(MapColor.GOLD).strength(0.8F).sound(SoundType.METAL).noOcclusion()));
+			new ChunkLoaderBlock(Block.Properties.of().mapColor(MapColor.GOLD).strength(0.8F).sound(SoundType.METAL).noOcclusion().pushReaction(PushReaction.BLOCK)));
 
 	public static final Supplier<BlockEntityType<ChunkLoaderBlockEntity>> CHUNK_LOADER_ENTITY = BLOCK_ENTITIES.register("chunk_loader", () ->
 			BlockEntityType.Builder.of(ChunkLoaderBlockEntity::new, CHUNK_LOADER.get()).build(null));
