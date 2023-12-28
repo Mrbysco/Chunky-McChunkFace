f0035a746e51b56c52dde40882ee4fce7498ec40
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-28 19:01:44 +0100

    Chunkloaders shouldn't be movable

--- src/main/java/com/mrbysco/chunkymcchunkface/datagen/data/ChunkyBlockTagProvider.java
+++ src/main/java/com/mrbysco/chunkymcchunkface/datagen/data/ChunkyBlockTagProvider.java
@@ -1,10 +1,14 @@
 package com.mrbysco.chunkymcchunkface.datagen.data;
 
 import com.mrbysco.chunkymcchunkface.ChunkyMcChunkFace;
+import com.mrbysco.chunkymcchunkface.registry.ChunkyRegistry;
 import com.mrbysco.chunkymcchunkface.registry.ChunkyTags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,8 +17,15 @@ public class ChunkyBlockTagProvider extends BlockTagsProvider {
 		super(generator, ChunkyMcChunkFace.MOD_ID, fileHelper);
 	}
 
+	public static final TagKey<Block> RELOCATION_NOT_SUPPORTED = forgeTag("relocation_not_supported");
+
+	private static TagKey<Block> forgeTag(String name) {
+		return BlockTags.create(new ResourceLocation("forge", name));
+	}
+
 	@Override
 	protected void addTags() {
 		this.tag(ChunkyTags.UPGRADE_BLOCKS).addTags(BlockTags.BEACON_BASE_BLOCKS);
+		this.tag(RELOCATION_NOT_SUPPORTED).add(ChunkyRegistry.CHUNK_LOADER.get());
 	}
 }
