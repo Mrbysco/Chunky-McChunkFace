0505a9dfc9a9bb5e28deeb3b8188b7a32bbd4005
Mrbysco <Mrbysco@users.noreply.github.com>
2023-12-28 19:06:26 +0100

    Don't allow piston movement

--- src/main/java/com/mrbysco/chunkymcchunkface/blocks/ChunkLoaderBlock.java
+++ src/main/java/com/mrbysco/chunkymcchunkface/blocks/ChunkLoaderBlock.java
@@ -27,6 +27,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -116,6 +117,11 @@ public class ChunkLoaderBlock extends BaseEntityBlock {
 				blockEntity.unloadChunks();
 			}
 		}
+	}
+
+	@Override
+	public PushReaction getPistonPushReaction(BlockState state) {
+		return PushReaction.BLOCK;
 	}
 
 	@Override
