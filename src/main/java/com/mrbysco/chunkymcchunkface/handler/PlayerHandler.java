b34dc6cb598dad27ec3c02dd2a80dc71da7bdff9
Mrbysco <Mrbysco@users.noreply.github.com>
2024-04-29 21:03:10 +0200

    Update NeoForge support

--- src/main/java/com/mrbysco/chunkymcchunkface/handler/PlayerHandler.java
+++ src/main/java/com/mrbysco/chunkymcchunkface/handler/PlayerHandler.java
@@ -3,9 +3,9 @@ package com.mrbysco.chunkymcchunkface.handler;
 import com.mrbysco.chunkymcchunkface.data.ChunkData;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 public class PlayerHandler {
 	@SubscribeEvent
@@ -29,8 +29,8 @@ public class PlayerHandler {
 	}
 
 	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		Player player = event.player;
+	public void onPlayerTick(PlayerTickEvent.Pre event) {
+		Player player = event.getEntity();
 		Level level = player.level();
 		if (!level.isClientSide && level.getGameTime() % 20L == 0L) {
 			ChunkData data = ChunkData.get(level);
